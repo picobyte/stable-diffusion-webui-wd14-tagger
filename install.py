26c388ce3eff536899a38a74aa1b8fe8b6835efe
Looong01 <lizelongdd@hotmail.com>
2024-12-16 21:37:06 +0100

    Add support for ROCm

--- install.py
+++ install.py
@@ -20,8 +20,8 @@ def check_rocm_version():
             return None
         result = subprocess.run(['hipcc', '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         for line in result.stdout.split('\n'):
-            if line.startswith('HIP version:'):
-                version = line.split()[2]
+            if 'HIP version:' in line:
+                version = line.split()[-1]
                 return version
     except FileNotFoundError:
         print("ROCm is not installed.")
@@ -53,13 +53,26 @@ def install_tensorflow_rocm(version):
             f"Couldn't install tensorflow-rocm {tf_version}.")
     else:
         print(f"Unsupported ROCm version: {version}. Please upgrade to ROCm 6.0.x, 6.1.x, 6.2.x, or 6.3.x")
-        return
+        return None
 
 # Check if the system has an AMD GPU and ROCm installed
-if 'AMD' in subprocess.run(['lspci'], stdout=subprocess.PIPE, text=True).stdout:
+if 'Radeon' in subprocess.run(['lspci'], stdout=subprocess.PIPE, text=True).stdout:
     rocm_version = check_rocm_version()
     if rocm_version:
-        install_tensorflow_rocm(rocm_version)
+        result = install_tensorflow_rocm(rocm_version)
+        if result is None:
+            print("Now install tensorflow for CPU instead.")
+            # Install tensorflow for CPU
+            run(f'"{sys.executable}" -m pip install tensorflow',
+                f"Installing tensorflow for CPU.",
+                f"Couldn't install tensorflow.")
+    else:
+        print("Please install ROCm to use AMD GPUs if you want to use GPU acceleration.")
+        print("Now install tensorflow for CPU instead.")
+        # Install tensorflow for CPU
+        run(f'"{sys.executable}" -m pip install tensorflow',
+            f"Installing tensorflow for CPU.",
+            f"Couldn't install tensorflow.")
 else:
     # Install tensorflow for CUDA
     run(f'"{sys.executable}" -m pip install tensorflow',
